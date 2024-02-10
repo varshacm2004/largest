7970085acf81c0965c0af4c8bdaf15fd4c102174
Varsha <01fe22bca019@kletech.ac.in>
2024-02-10 12:45:12 +0530

    program for mathfunction

---
+++ mathfunction.java
@@ -0,0 +1,11 @@
+
+class mathfunction {
+    public static void main(String args[]) {
+        System.out.println("maximum is " + Math.max(5, 19));
+        System.out.println("minimum is " + Math.min(7, 1));
+        System.out.println("square root is " + Math.sqrt(81));
+        System.out.println("cube root is " + Math.cbrt(812));
+        System.out.println("random " + Math.random());
+        System.out.println("absolute is " + Math.abs(2.4678));
+    }
+}
